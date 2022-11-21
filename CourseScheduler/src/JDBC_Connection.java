597626c6124f595e8440ce2caf1b1a4f3aa1a3ef
Jun_Park <jun.park3@mytwu.ca>
2022-11-21 01:04:00 -0800

    Add .sql file (TEST)

---
+++ CourseScheduler/src/JDBC_Connection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBC_Connection {
+
+    public static void main(String[] args) {
+        //https://www.youtube.com/watch?v=e8g9eNnFpHQ
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/courses", "root", "1234");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from Student");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("Student_id"));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
