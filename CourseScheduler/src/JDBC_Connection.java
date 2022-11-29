a949cf2b0d03bd27de960ce067a5b860ffc7a1b9
Jun_Park <jun.park3@mytwu.ca>
2022-11-29 01:46:33 -0800

    sqlconnection

--- CourseScheduler/src/JDBC_Connection.java
+++ CourseScheduler/src/JDBC_Connection.java
@@ -8,11 +8,11 @@ public class JDBC_Connection {
     public static void main(String[] args) {
         //https://www.youtube.com/watch?v=e8g9eNnFpHQ
         try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/courses", "root", "1234");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/D62PAbWL1b", "D62PAbWL1b", "pT9OQggEJ3");
 
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("select * from Student");
+            ResultSet resultSet = statement.executeQuery("select * from STUDENT");
 
             while (resultSet.next()){
                 System.out.println(resultSet.getString("Student_id"));
