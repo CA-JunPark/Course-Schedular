238c77dc10d1426fe33d71803fdb4a1cee75e4f5
Jun_Park <jun.park3@mytwu.ca>
2022-12-05 21:25:15 -0800

    merge final 9:25

--- CourseScheduler/src/application/Controller.java
+++ CourseScheduler/src/application/Controller.java
@@ -1,24 +1,28 @@
 package application;
 
-import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuButton;
+import javafx.scene.control.Menu;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.util.Duration;
+import javafx.stage.StageStyle;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,7 +37,13 @@ import java.sql.Statement;
 public class Controller {
 
     @FXML
-    private MenuBar MenuBar;
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Menu menuFile;
+
+    @FXML
+    private Menu menuHelp;
 
     @FXML
     private AnchorPane anchorPaneCalendar;
@@ -68,16 +78,7 @@ public class Controller {
     boolean isClicked = false;
 
     public static Stage stage;
-    // String[] C1 = new String[]{"CMPT 211 A","CMPT 360 A","CMPT 380 A","CMPT 470 A"};
-    // String[] C2 = new String[]{"Web Programming I", "Comparative Programming Languages", "Artificial Intelligence", "Introduction to Bioinformatics"};
-    // String[] T = new String[]{"18:15~21:00", "09:30~10:45", "15:00~16:15", "12:00~14:45"};
-    // String[][] W = new String[][]{{"R"},{"M","W","F"},{"T","R"},{"F"}};
-    // String[] D = new String[]{"An introduction to web application development using current web technologies, best practices, and standards. The modern web application framework will be studied.",
-    //         "The history, development, and design principles for programming notations. The design and internal operations of the major notational categories are examined in detail. Students are expected to become proficient in at least four languages they have not previously learned, typically chosen from historical, modern working, and cutting edge languages and from among such (non-exclusive) categories as Algol-descended, functional scripting, Web-based, modular, application-specific, visual, and object oriented. They will also learn how to select appropriate programming notations for a given project. Programming will be undertaken in at least three OS environments.",
-    //         "Artificial Intelligence: knowledge representation, logic programming, knowledge inference. Application domains within the dicisipline of Artificial Intelligence include logical and probabilistic reasoning, natural language understanding, vision and expert systems.",
-    //         "An overview of the interdisciplinary science of genomics, proteomics, and bioinformatics which applies the tools of information technology (computer hardware and software) to analyze biological data such as gene or protein sequences. This course examines the theory of bioinformatics as well as its practical application to biological problems using approaches such as BLAST searches, phylogenetics, and protein structure function analysis."};
-    // String[] P = new String[]{"Mcbride, Ryan","Sutcliffe, Richard Joseph","Park, Andrew Jaehyung","Tsang, Herbert H" };
-
+    
     // last clicked index in the listView
     static int index = 0;
 
@@ -89,20 +90,6 @@ public class Controller {
     static ArrayList<String> P = new ArrayList<>();
     public void initialize(){
         setTimeLines();
-        // addCourse(C1[0], getStartTimeFromData(T[0]), getEndTimeFromData(T[0]), getDayOfWeekFromData(W[0][0]), "#4D66CC");
-
-        // addCourse(C1[1], getStartTimeFromData(T[1]), getEndTimeFromData(T[1]), getDayOfWeekFromData(W[1][0]), "#664DB3");
-        // addCourse(C1[1], getStartTimeFromData(T[1]), getEndTimeFromData(T[1]), getDayOfWeekFromData(W[1][1]), "#664DB3");
-        // addCourse(C1[1], getStartTimeFromData(T[1]), getEndTimeFromData(T[1]), getDayOfWeekFromData(W[1][2]), "#664DB3");
-
-        // addCourse(C1[2], getStartTimeFromData(T[2]), getEndTimeFromData(T[2]), getDayOfWeekFromData(W[2][0]), "#FF9966");
-        // addCourse(C1[2], getStartTimeFromData(T[2]), getEndTimeFromData(T[2]), getDayOfWeekFromData(W[2][1]), "#FF9966");
-
-        // addCourse(C1[3], getStartTimeFromData(T[3]), getEndTimeFromData(T[3]), getDayOfWeekFromData(W[3][0]), "#FF6666");
-
-        // for (int i=0; i<C1.length; i++) {
-        //     listView.getItems().add(C1[i]+": "+C2[i]);
-        // }
 
         ResultSet a = JDBC_Connection.initialSearch();
         
@@ -169,6 +156,53 @@ public class Controller {
             scrollPane.setPrefHeight(612);
             isClicked = true;
         }
+    }
+    @FXML
+    void processMenuFile(ActionEvent event) {
+        // USER INFO
+        menuFile.getItems().get(0).setOnAction(e->{
+
+        });
+        // EXPORT
+        menuFile.getItems().get(1).setOnAction(e->{
+            WritableImage image = anchorPaneCalendar.snapshot(null, null);
+            ImageView view = new ImageView(image);
+            Stage stage = new Stage();
+            AnchorPane root = new AnchorPane();
+            Scene scene = new Scene(root, image.getWidth(),image.getHeight());
+            stage.setScene(scene);
+            stage.show();
+            stage.setResizable(false);
+            root.getChildren().add(view);
+        });
+        // EXIT
+        menuFile.getItems().get(2).setOnAction(e->{
+            Stage stage = (Stage) anchorPane.getScene().getWindow();
+            stage.close();
+        });
+
+    }
+
+    @FXML
+    void processMenuHelp(ActionEvent event) {
+        System.out.println("HELP");
+        // ABOUT
+        menuHelp.getItems().get(0).setOnAction(e->{
+            Stage stage = new Stage();
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("aboutPage.fxml")));
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            assert root != null;
+            Scene scene = new Scene(root, 480, 320);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("application.css")).toExternalForm());
+            stage.setScene(scene);
+            stage.setTitle("About");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+        });
     }
 
 
