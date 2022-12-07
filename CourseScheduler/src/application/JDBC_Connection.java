8c4cc8602aa3b0af79f1202215e39b616cbc6012
Linyun Liu <90886678+LinyunLiu@users.noreply.github.com>
2022-12-06 23:25:25 -0800

    linyun commit Dec 6

--- CourseScheduler/src/application/JDBC_Connection.java
+++ CourseScheduler/src/application/JDBC_Connection.java
@@ -12,34 +12,34 @@ public class JDBC_Connection {
 
     
     //https://www.youtube.com/watch?v=e8g9eNnFpHQ
-    public static void main(String[] args) {
-        ResultSet a = initialSearch();
-        ArrayList<String> C1 = null;
-        ArrayList<String> C2 = null;
-        ArrayList<String> T = null;
-        ArrayList<String[]> W = null;
-        ArrayList<String> D = null;
-        ArrayList<String> P = null;
-        try{
-            while(a.next()){
-                String code = a.getString("CourseCode") + " " + a.getString("Section");
-                C1.add(code);
-                C2.add(a.getString("CourseTitle"));
-                String time = a.getString("_Time");
-                String[] t = time.split(" ");
-                T.add(t[0]+t[2]+t[3]);
-                String date = a.getString("_Date");
-                String[] d = date.split("");
-                W.add(d);
-                D.add(a.getString("_DEscription"));
-                P.add(a.getString("Instructor"));
-            }
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-        
-    }
+//    public static void main(String[] args) {
+//        ResultSet a = initialSearch();
+//        ArrayList<String> C1 = null;
+//        ArrayList<String> C2 = null;
+//        ArrayList<String> T = null;
+//        ArrayList<String[]> W = null;
+//        ArrayList<String> D = null;
+//        ArrayList<String> P = null;
+//        try{
+//            while(a.next()){
+//                String code = a.getString("CourseCode") + " " + a.getString("Section");
+//                C1.add(code);
+//                C2.add(a.getString("CourseTitle"));
+//                String time = a.getString("_Time");
+//                String[] t = time.split(" ");
+//                T.add(t[0]+t[2]+t[3]);
+//                String date = a.getString("_Date");
+//                String[] d = date.split("");
+//                W.add(d);
+//                D.add(a.getString("_DEscription"));
+//                P.add(a.getString("Instructor"));
+//            }
+//        }
+//        catch(Exception e){
+//            e.printStackTrace();
+//        }
+//
+//    }
 
     public static ResultSet initialSearch(){
         ResultSet resultSet = null;
